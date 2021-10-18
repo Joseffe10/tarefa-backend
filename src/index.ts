4ba509414eaeee9ed3b0baf37de42db28ad3a692
joseffe10 <joseffe_oliveira@whirlpool.com>
2021-10-18 17:12:34 -0300

    Deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import "reflect-metadata";
-import database from "./database";
+//import database from "./database";
+import { createConnection } from "typeorm";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import routes from "./routes";
 
 const app = express()
+createConnection()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('🏃 Running Server');
-})
+});
 
+/*
 database
   .createDatabaseConnection("local")
   .then(async (connection) => {
@@ -22,4 +25,5 @@ database
   .catch((error) => {
     console.log(error)
   })
+  */
 
