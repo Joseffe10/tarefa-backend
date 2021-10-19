0f9f943d99474377eee86c6cc44e33ffebfdc316
joseffe10 <joseffe_oliveira@whirlpool.com>
2021-10-19 11:40:29 -0300

    Deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,17 @@
 import "reflect-metadata";
-import database from "./database";
-//import { createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import routes from "./routes";
 
-var path = require('path')
-var serveStatic = require('serve-static')
-
 const app = express()
-//createConnection()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+createConnection()
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
-
+ 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('🏃 Running Server');
+  console.log('Servidor em Execução');
 });
-
-database
-  .createDatabaseConnection("local")
-  .then(async (connection) => {
-    console.log('Conectado com o BD Postgres!')
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-
