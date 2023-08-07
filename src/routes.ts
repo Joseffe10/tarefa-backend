e639bd21cf9782faa94debd14125294acf2f41ec
joseffe10 <joseffe_oliveira@whirlpool.com>
2023-08-07 16:44:50 -0300

    Ajuste na mensagem da rota home

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,7 @@ import { finishedTask } from './controller/TasksController';
 const routes = Router()
  
 routes.get('/home', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello Turma 007!' })
+    return response.json({ message: 'Hello Turma!!!' })
 })
 
 routes.get('/tasks', getTasks)
